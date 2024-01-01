633932455fc0434915527d644023a287256f96b7
Darren Shepherd <darren@acorn.io>
2024-01-01 11:33:52 -0700

    Updates

--- go.mod
+++ go.mod
@@ -5,14 +5,19 @@ go 1.21.5
 replace github.com/acorn-io/cmd => ../cmd
 
 require (
+	github.com/acorn-io/cmd v0.0.0-20231020234559-285455daa0d8
+	github.com/cosmtrek/air v1.49.0
+	github.com/ramr/go-reaper v0.2.1
+	github.com/spf13/cobra v1.7.0
+)
+
+require (
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/acorn-io/baaah v0.0.0-20231009165317-af2b68361b8a // indirect
-	github.com/acorn-io/cmd v0.0.0-20231020234559-285455daa0d8 // indirect
 	github.com/bep/godartsass v0.16.0 // indirect
 	github.com/bep/golibsass v1.1.0 // indirect
 	github.com/bombsimon/logrusr/v4 v4.0.0 // indirect
 	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cosmtrek/air v1.49.0 // indirect
 	github.com/creack/pty v1.1.18 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
@@ -24,12 +29,13 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/ramr/go-reaper v0.2.1 // indirect
+	github.com/samber/lo v1.38.1 // indirect
+	github.com/samber/slog-logrus v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tdewolff/parse/v2 v2.6.5 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
---
+++ pkg/engine/run.go
@@ -0,0 +1,119 @@
+package engine
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/cosmtrek/air/runner"
+)
+
+type Options struct {
+	AirConfig string `usage:"air config to override defaults" short:"c"`
+}
+
+func Run(ctx context.Context, args []string, opt Options) error {
+	cfg, err := runner.InitConfig(opt.AirConfig)
+	if err != nil {
+		return err
+	}
+
+	if _, err := exec.LookPath("dlv"); errors.Is(err, fs.ErrNotExist) {
+		cmd := exec.Command("go", "install", "github.com/go-delve/delve/cmd/dlv@latest")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
+	if err := setDefaults(cfg, args); err != nil {
+		return err
+	}
+
+	r, err := runner.NewEngineWithConfig(cfg, false)
+	if err != nil {
+		return err
+	}
+	context.AfterFunc(ctx, r.Stop)
+
+	r.Run()
+	return nil
+}
+
+func setSliceIfEmpty[T any](currentValue, newValue []T) []T {
+	if len(currentValue) > 0 {
+		return currentValue
+	}
+	return newValue
+}
+
+func setIfEmpty[T comparable](currentValue, newValue T) T {
+	var unset T
+	if currentValue == unset {
+		return newValue
+	}
+	return currentValue
+}
+
+func setDefaults(cfg *runner.Config, args []string) error {
+	tmp, err := os.MkdirTemp("", "air-run")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tmp)
+
+	log.Printf("Using temp dir: %s", tmp)
+
+	var (
+		targets []string
+		argsBin []string
+	)
+
+	for i, arg := range args {
+		if arg == "--" {
+			argsBin = args[i+1:]
+			break
+		}
+		if _, err := os.Stat(arg); err == nil {
+			targets = append(targets, arg)
+		} else {
+			argsBin = args[i:]
+			break
+		}
+	}
+
+	if len(targets) == 0 {
+		targets = []string{"."}
+	}
+
+	bin := filepath.Join(tmp, "main")
+	cmd := fmt.Sprintf("go build -o %s %s", bin, strings.Join(targets, " "))
+	log := filepath.Join(tmp, "build-errors.log")
+	fullBin := fmt.Sprintf("dlv exec --continue --accept-multiclient --listen=:2345 --headless=true --api-version=2 --log %s --", bin)
+
+	cfg.Build.ArgsBin = setSliceIfEmpty(cfg.Build.ArgsBin, argsBin)
+	cfg.Build.Bin = setIfEmpty(cfg.Build.Bin, bin)
+	cfg.Build.Cmd = setIfEmpty(cfg.Build.Cmd, cmd)
+	cfg.Build.Delay = setIfEmpty(cfg.Build.Delay, 1000)
+	cfg.Build.ExcludeDir = setSliceIfEmpty(cfg.Build.ExcludeDir, []string{"assets", "tmp", "testdata"})
+	cfg.Build.ExcludeRegex = setSliceIfEmpty(cfg.Build.ExcludeDir, []string{"_test.go"})
+	cfg.Build.FullBin = setIfEmpty(cfg.Build.FullBin, fullBin)
+	cfg.Build.KillDelay = setIfEmpty(cfg.Build.KillDelay, time.Second)
+	cfg.Build.Log = setIfEmpty(cfg.Build.Log, log)
+	cfg.Build.SendInterrupt = true
+	cfg.Build.StopOnError = true
+	cfg.Build.Rerun = true
+	cfg.Build.RerunDelay = setIfEmpty(cfg.Build.RerunDelay, 5000)
+
+	return nil
+}
